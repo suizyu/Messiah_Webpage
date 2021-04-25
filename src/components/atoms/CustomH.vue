36e9928252e1838d80ca00850d3c974b4cb4e636
suizyu <suizyu_m@yahoo.co.jp>
2021-04-25 19:21:40 +0900

    [CustomH] 実装。

---
+++ src/components/atoms/CustomH.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.component('custom-heading',{
+    render: function(createElement) {
+        return createElement(
+            'h' + this.level,
+            [
+                this.$slots.default, 
+                this.decorate ?
+                    createElement(
+                        'img', 
+                        { 
+                            attrs: { src: require('../../assets/images/heading-border-pc.png') },
+                            class:  "border-decorate"
+                        }
+                    ) :
+                    ''
+            ]
+        )
+    },
+    props:{
+        level: {
+            type: Number,
+            required: true
+        },
+        decorate: {
+            type: Boolean,
+            default: false,
+            required: false
+        }
+    },
+    data() {
+        return {
+            imagePath: require("../../assets/images/heading-border-pc.png")
+        }
+    }
+})
+</script>
+<style>
+    .border-decorate {
+        display: block;
+        position: inherit;
+        height: 13vh;
+        max-height: 45px;
+    }
+    h1, h2, h3, h4, h5, h6 {
+        color: white;
+        text-align: center;
+        position: relative;
+    }
+    img {
+        margin: 0 auto;
+        position: absolute;
+        bottom: 7px;
+    }
+
+</style>
---
+++ src/components/stories/atoms/CutomH.stories.ts
@@ -0,0 +1,35 @@
+import CustomH from "../../atoms/CustomH.vue";
+
+export default {
+    title: "atoms/CustomH",
+}
+
+const Template = (args: any, { argTypes }: any) => ({
+    props: Object.keys(argTypes),
+    components: { CustomH },
+    template: `<CustomH v-bind="$props"> Title </CustomH>`
+})
+
+export const h1 = Template.bind({})
+h1.args = {
+    level: 1,
+    decorate: false
+}
+
+export const h1_decorate = Template.bind({})
+h1_decorate.args = {
+    level: 1,
+    decorate: true
+}
+
+export const h2 = Template.bind({})
+h2.args = {
+    level: 2,
+    decorate: false
+}
+
+export const h3 = Template.bind({})
+h3.args = {
+    level: 3,
+    decorate: false
+}
