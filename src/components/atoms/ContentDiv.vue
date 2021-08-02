becb01b1b88bdffbf976edd9dbd140cdd689e779
suizyu <suizyu_m@yahoo.co.jp>
2021-08-03 02:45:14 +0900

    [ContentDiv][CustomSection]実装。

---
+++ src/components/atoms/ContentDiv.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="content">
+        <slot />
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+})
+</script>
+<style scoped>
+    .content {
+        margin: 0 auto;
+        width: 80vw;
+        max-width: 800px;
+        background: hotpink;
+        padding: 10px 0;
+    }
+</style>
---
+++ src/components/atoms/CustomSection.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="custom-section">
+        <slot />
+    </section>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+})
+</script>
+<style scoped>
+    .custom-section {
+        margin: 10vh 0;
+        width: 100%;
+    }
+</style>
