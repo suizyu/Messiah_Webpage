8745123c352ef503cc9cbc880dc0194ce3131ddc
suizyu <suizyu_m@yahoo.co.jp>
2021-08-22 02:54:35 +0900

    [KeywordTips] 実装。

---
+++ src/components/organisms/KeywordTips.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="tips">
+        <TipsAccordion v-for="item in items" 
+            :key="item.id"
+            :subTitle="item.subtitle">
+            <template #summary>{{ item.title }}</template>
+            <template #detailText><div v-html="item.detail"></div></template>
+        </TipsAccordion>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import TipsAccordion from '../molecules/TipsAccordion.vue';
+export default Vue.extend({
+    props: ['items'],
+    components: {
+        TipsAccordion
+    }
+})
+</script>
+<style scoped>
+    .tips {
+        width: 100%;
+        margin: 10px auto;
+    }
+</style>
