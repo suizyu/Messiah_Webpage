2da54695f8c7e1549a4bae8c40003fe09fd4406a
suizyu <suizyu_m@yahoo.co.jp>
2021-08-21 18:01:47 +0900

    [DetailButton] nuxt-link, aタグの切替え、色・disableパラメータ実装。

--- src/components/stories/atoms/DetailButton.stories.ts
+++ src/components/stories/atoms/DetailButton.stories.ts
@@ -4,7 +4,28 @@ export default {
     title: "atoms/DetailButton",
 }
 
-export const detail_buttons = () => ({
+const Template = (args: any, { argTypes }: any) => ({
+    props: Object.keys(argTypes),
     components: { DetailButton },
-    template: "<DetailButton />"
+    template: '<DetailButton v-bind="$props" />'
 })
+
+export const nuxt_link = Template.bind({})
+nuxt_link.args = {
+    link: "/introduction/",
+    color: "",
+    disable: false
+}
+
+export const anchor_link = Template.bind({})
+anchor_link.args = {
+    link: "https://twitter.com/home",
+    color: "",
+    disable: false
+}
+
+export const disable = Template.bind({})
+disable.args = {
+    link: "https://twitter.com/home",
+    disable: true
+}
