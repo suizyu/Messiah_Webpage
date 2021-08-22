60b5f49932ad45104ce74a4a03756c00799e485a
suizyu <suizyu_m@yahoo.co.jp>
2021-08-23 00:09:29 +0900

    [CharactorLabel] 実装。

---
+++ src/components/atoms/CharactorLabel.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="caractor-label">
+        <div class="name-block">
+            <span class="name">{{ name }}</span>
+            <span class="voice">CV：{{ voiceActor }}</span>
+        </div>
+        <hr />
+        <span class="spelling">{{ spelling }}</span>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        name: {
+            type: String,
+            required: false,
+            default: "デフォルトネーム"
+        },
+        spelling: {
+            type: String,
+            required: false,
+            default: "Default Name"
+        },
+        voiceActor: {
+            type: String,
+            required: false,
+            default: "なし"
+        }
+    }
+})
+</script>
+<style scoped>
+    .caractor-label {
+        color: #fff;
+        text-align: left;
+        width: 45%;
+    }
+    .caractor-label hr {
+        border-width: 1px;
+        border-style: solid;
+        border-color: #fff;
+        margin: 0;
+    }
+    .name-block {
+        display: flex;
+        align-items: baseline;
+    }
+    .name-block span {
+        margin-right: 30px;
+    }
+    .name {
+        font-size: 2.5rem;
+        font-family: kinuta-maruminshinano-stdn, serif;
+        font-feature-settings: "palt";
+    }
+    .spelling {
+        font-family: classico-urw, sans-serif;
+        font-size: 1.2rem;
+    }
+</style>
---
+++ src/components/stories/atoms/CharactorLabel.stories.ts
@@ -0,0 +1,25 @@
+import CharactorLabel from "../../atoms/CharactorLabel.vue";
+
+export default {
+    title: "atoms/CharactorLabel",
+}
+
+const Template = (args: any, { argTypes }: any) => ({
+    props: Object.keys(argTypes),
+    components: { CharactorLabel },
+    template: `<CharactorLabel v-bind="$props" />`
+})
+
+export const default_state = Template.bind({})
+default_state.args = {
+    name: "デフォルトネーム",
+    spelling: "Default Name",
+    voiceActor: "なし"
+}
+
+export const ex_reif = Template.bind({})
+ex_reif.args = {
+    name: "リーフ",
+    spelling: "Rih",
+    voiceActor: "--"
+}
