c1313daf2494c0c7545d895e81ba233aeecf974a
suizyu <suizyu_m@yahoo.co.jp>
2021-12-06 00:09:02 +0900

    [GarellyImg] client-onlyタグ追加。

--- src/components/atoms/GarellyImg.vue
+++ src/components/atoms/GarellyImg.vue
@@ -1,20 +1,25 @@
 <template>
     <div class="image-box">
-        <Tinybox v-model="imageIndex" 
-            :images="imageData()"
-            :no-thumbs="true">
-        </Tinybox>
-        <img v-for="img in imageData()" 
-            :key="img.id"
-            :src="img.thumb" 
-            class="open-tinybox thumbnail" 
-            @click="imageIndex = img.id">
+        <client-only>
+            <Tinybox v-model="imageIndex" 
+                :images="imageData()"
+                :no-thumbs="true">
+            </Tinybox>
+            <img v-for="img in imageData()" 
+                :key="img.id"
+                :src="img.thumb" 
+                class="open-tinybox thumbnail" 
+                @click="imageIndex = img.id">
+        </client-only>
     </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 import Tinybox from "vue-tinybox";
 export default Vue.extend({
+    components: {
+        Tinybox
+    },
     props: {
         imgName: {
             type: String,
@@ -32,9 +37,6 @@ export default Vue.extend({
             imageIndex: null
         }
     },
-    components: {
-        Tinybox
-    },
     methods: {
         imageData() {
             return [
@@ -51,8 +53,5 @@ export default Vue.extend({
 <style scoped>
     .thumbnail:hover {
         cursor: pointer;
-    }
-    .image-box {
-        cursor: default;
     }
 </style>
