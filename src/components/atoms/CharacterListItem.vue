4a08a5038d0b6c6cfa8e1d4bd27028c24ad8fbf3
suizyu <suizyu_m@yahoo.co.jp>
2021-12-06 00:07:19 +0900

    [CharacterList][CharacterListItem] 実装。

---
+++ src/components/atoms/CharacterListItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <nuxt-link :to="toPath()" class="character-img">
+        <img :src="imagePath()" 
+            :alt="this.imgKey" />
+    </nuxt-link>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        imgKey: {
+            type: String,
+            required: false,
+            default: "elvis"
+        },
+        name: {
+            type: String,
+            required: false,
+            default: "エルヴィス"
+        }
+    },
+    methods: {
+        imagePath() {
+            return require(`~/assets/images/character/_list/${ this.imgKey }.png`);
+        },
+        toPath() {
+            return "/character/" + this.imgKey;
+        }
+    }
+})
+</script>
+<style scope>
+    .character-img {
+         max-width: 300px;
+         margin: 10px;
+         transition: .5s;
+    }
+    .character-img:hover {
+        opacity: .7;
+        mix-blend-mode: hard-light;
+    }
+</style>
---
+++ src/components/pageContents/character/CharacterList.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="list-wrapper">
+        <div class="cahacter-list">
+            <template v-for="chara in characters">
+                <CharacterListItem v-bind="chara" :key="chara.order"/>
+            </template>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import CharacterListItem from '../../atoms/CharacterListItem.vue'
+export default Vue.extend({
+    components: {
+        CharacterListItem
+    },
+    props: {
+        effect: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+    data() {
+        return {
+            characters: [
+                {
+                    imgKey: "elvis",
+                    name: "エルヴィス",
+                    order: 0
+                },
+                {
+                    imgKey: "rih",
+                    name: "リーフ",
+                    order: 1
+                }
+            ]
+        }
+    },
+    mounted() {
+        if (this.effect) {
+            this.Animation();
+        }
+    },
+    methods: {
+        Animation() {
+            console.log("demo");
+        }
+    }
+})
+</script>
+<style scoped>
+    .list-wrapper {
+        display: block;
+        margin: 0 auto;
+    }
+    .cahacter-list{
+        display: flex;
+        justify-content: center;
+    }
+</style>
